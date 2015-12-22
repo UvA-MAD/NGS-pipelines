ce675e972d7ccd3dc0e8d49b4c865b8e640066ef
Wim de Leeuw <w.c.deleeuw@uva.nl>
2015-12-22 15:06:32 +0100

    Merged mrna Snakefiles for eukaryotes and prokaryotes. Introduced a parameter file for mrna pipeline: Snakeparam_mrna.py which contains the configurable parts of the pipeline

--- Snakeparam_mrna.py
+++ Snakeparam_mrna.py
@@ -1,3 +1,4 @@
+
 import os
 
 #domain for which pipeline is run pick use "prokaryote"  or "eukaryote"
@@ -6,11 +7,14 @@ DOMAIN= "xxx"
 # species for which pipeline is run
 SPECIES = "xxx"
 
+# fasta file containing 
 # in an ideal world the database is found by species alone 
-GENOME_DB="/xxx/xxx.dna.genome.fa"
+GENOME_DB="/xxx/xxx/xxx.fasta" 
+
 
 # fasta file with transcriptome: mandatory for eukaryotes not used for prokaryotes
-TRANSCRIPTOME_DB="/xxx/xxx.transcripts.fa"
+TRANSCRIPTOME_DB="/xxx/xxx/xxx.fa"
+
 
 #  genome annotation with transcriptome: mandatory for prokaryotes not used for eukaryotes
 GENOME_ANNOT="/xxx/xxx.gff"
@@ -24,10 +28,13 @@ FQ_DIR = os.path.join(EXPERIMENT_DIR,"Scratch/raw")
 # samples to be processed if unchanged: all files with fastq extention in FQ_DIR
 SAMPLES =  [s[:-6] for s in os.listdir(FQ_DIR) if s.endswith(".fastq")]
 
-# map to use for temporary files. Removal is not automatic  
-SCRATCH_DIR = os.path.join(EXPERIMENT_DIR,"Scratch/mapped")
+# map to use for temporary files.
+# Removal is not automatic  
+# snakemake 
+SCRATCH_DIR = os.path.join(EXPERIMENT_DIR,"Scratch/mrna_map")
+
 # map with scratch results 
-RESULT_DIR = os.path.join(EXPERIMENT_DIR,"Results/mapping")
+RESULT_DIR = os.path.join(EXPERIMENT_DIR,"Results/mrna_map")
 
 # reference spike-in sequences
 SPIKES_REF = "/mad/MAD-RBAB/05_Reference-db/external/ERCC/ERCC92.fa"
