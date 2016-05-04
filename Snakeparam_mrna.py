b29ac69c6b2c1ebd9520dd35022a2b6eb4bf6965
Wim de Leeuw <w.c.deleeuw@uva.nl>
2016-05-04 11:42:41 +0200

    mRNA pipeline extended    1. downloading genome.    2. parameterized trimming    3. htseq-count cleanig post-processing (remove last 5 lines)

--- Snakeparam_mrna.py
+++ Snakeparam_mrna.py
@@ -11,13 +11,18 @@ SPECIES = "xxx"
 # in an ideal world the database is found by species alone 
 GENOME_DB="/xxx/xxx/xxx.fasta" 
 
+# url which points to genome (used if GENOME_DB is not found) 
+GENOME_FASTA_URL="xxx://xxx.yyy.zzz/abc"
+
 
 # fasta file with transcriptome: mandatory for eukaryotes not used for prokaryotes
 TRANSCRIPTOME_DB="/xxx/xxx/xxx.fa"
 
-
 #  genome annotation with transcriptome: mandatory for prokaryotes not used for eukaryotes
 GENOME_ANNOT="/xxx/xxx.gff"
+
+# url which points to genome annotation (used if GENOME_ANNOT is not found) 
+GENOME_GFF_URL="xxx://xxx.yyy.zzz/abc"
 
 #location of the experiment (variable only used in this file) 
 EXPERIMENT_DIR = "../.."
@@ -39,3 +44,9 @@ RESULT_DIR = os.path.join(EXPERIMENT_DIR,"Results/mrna_map")
 # reference spike-in sequences
 SPIKES_REF = "/mad/MAD-RBAB/05_Reference-db/external/ERCC/ERCC92.fa"
 
+
+# triming of reads. 
+# reads shorter than MIN_READLEN wil be removed from analysis 
+# reads longer than MAX_READLEN will be tail clipped to a length of MAX_READLEN
+MIN_READLEN ="25"
+MAX_READLEN ="50"
