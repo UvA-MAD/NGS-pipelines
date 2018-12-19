366104bb76639e57181a6cb43c18a161fdcf0b93
Wim de Leeuw <w.c.deleeuw@uva.nl>
2018-12-19 14:58:37 +0100

    Illumina data added to get_experiment_fq.py.

--- get_experiment_fq.py
+++ get_experiment_fq.py
@@ -6,12 +6,42 @@ import os
 import sys
 
 
-def sample_bam2fq(sample_info):
+proton_run_store   = "/mad/MAD-RBAB/04_MAD-RBAB-runs/data"
+illumina_run_store = "/mad/MAD-RBAB/04_MAD-RBAB-runs/illumina/data"
+
+def illumina_gather_sample(sample_info):
+    """
+    For each sampleid generate fastq file(s).
+    """
+    sample_data, output_dir = sample_info
+    sampleid = list(sample_data['sampleid'])[0]
+    for pend in ["1","2"] : 
+      file_pattern = '%s_S[1-9][0-9]*_R%s_001.fastq.gz'
+      sample_gz = []
+      for index, row in sample_data.iterrows():
+          iid = row['runid']
+          fa_gz_regexp = re.compile(file_pattern % (sampleid,pend))
+          subdir, dirs, files = next(os.walk(os.path.join(illumina_run_store, iid)))
+          for f in files:
+              if fa_gz_regexp.match(f):
+                  gz_file = os.path.join(illumina_run_store, iid, f)
+                  sample_gz.append(gz_file)
+      sample_fastq = os.path.join(output_dir, str(sampleid) + '_R'+pend+'.fastq.gz')
+      if (len(sample_gz) > 0) : 
+         zcat_command = ['zcat'] + sample_gz
+         zip_command = ['gzip','-c'] 
+         ps = subprocess.Popen(zcat_command,stdout=subprocess.PIPE)
+         with open(sample_fastq, 'w') as fout:
+            subprocess.call(zip_command,stdin=ps.stdout,stdout=fout)
+            ps.wait()
+
+
+def proton_sample_bam2fq(sample_info):
     """
     For each sampleid generate fastq file(s) from bam.
     """
     # unpack arguments
-    sample_data, bam_home, output_dir = sample_info
+    sample_data, output_dir = sample_info
     # patttern of bam file names
     bam_pattern = '^IonXpress(RNA)?_0%s_rawlib.(basecaller.)?bam$'
     sampleid = list(sample_data['sampleid'])[0]
@@ -19,12 +49,12 @@ def sample_bam2fq(sample_info):
     for index, row in sample_data.iterrows():
         rid = row['runid']
         bc = row['barcode'][2:]
-        subdir, dirs, files = next(os.walk(os.path.join(bam_home, rid)))
+        subdir, dirs, files = next(os.walk(os.path.join(proton_run_store, rid)))
 
         bam_regexp = re.compile(bam_pattern % bc)
         for f in files:
             if bam_regexp.match(f):
-                bam_file = os.path.join(bam_home, rid, f)
+                bam_file = os.path.join(proton_run_store, rid, f)
                 sample_bams.append(bam_file)
 
     # generate fastq files from bam files for this sample
@@ -49,27 +79,22 @@ def sample_bam2fq(sample_info):
     # remove fastq  files from before concatenation
     [os.remove(f) for f in sample_fastqs]
 
+
+
 def main(argv):
     # setup argument parse
     parser = argparse.ArgumentParser(
-        description="Generate fastq files from bam files.",
-        epilog="Finds bam files for samples described in design file."
+        description="Generate per sample fastq files from sequencer output.",
+        epilog="Finds files for samples described in design file."
                "If sample has been run in muliple runs, fq files are combine to one."
                "Generated fastq files are placed in 'output_dir'.")
 
     parser.add_argument('-d', '--design_file',
                         type=argparse.FileType('r'),
                         help="path to design file")
-    parser.add_argument('-s', '--bam_home',
-                        type=str,
-                        help="path to original bam files")
     parser.add_argument('-a', '--output_dir',
                         type=str,
                         help="path to target directory with analysis")
-    parser.add_argument('-n', '--numthreads',
-                        default=1,
-                        type=int,
-                        help="number of threads, max 10")
 
     # print help if no command line arguments have been used
     if len(sys.argv) == 1:
@@ -85,7 +110,7 @@ def main(argv):
     # first three column names should be case insensitive
     # therefore convert to lower case
     colnames = list(df.columns)
-    colnames[:3] = [s.lower() for s in colnames[:3]]
+    colnames = [s.lower() for s in colnames]
     df.columns = colnames
 
     # get unique sample names
@@ -96,26 +121,22 @@ def main(argv):
     if not os.path.exists(args.output_dir):
         os.makedirs(args.output_dir)
 
-    numthreads = int(args.numthreads)
-    if numthreads > 1:
-        # max threads is 10
-        n = (10 if numthreads > 10 else numthreads)
-        import multiprocessing
-        arguments = []
-        for sampleid in samples:
-            # rows corresponding to particular sample
-            # more than one if sample has been run in multiple runs
-            sample_data = df[df['sampleid'] == sampleid]
-            arguments.append((sample_data,
-                              args.bam_home,
-                              args.output_dir))
-
-        pool = multiprocessing.Pool(processes=n)
-        pool.map(sample_bam2fq, arguments)
+# Now find out the type of experiment Proton or illumina 
+    idtypes=list(set([s[0:3] for s in df['runid']]))
+    if len(idtypes) != 1: 
+        print("ERROR: get_experiment.py currently can not handle multi-platform designs."); 
+        sys.exit(1)
+    if idtypes == ["RID"]: 
+       for sampleid in samples:
+           sample_data = df[df['sampleid'] == sampleid]
+           proton_sample_bam2fq((sample_data, args.output_dir))
+    elif idtypes == ["IID"]:
+       for sampleid in samples:
+           sample_data = df[df['sampleid'] == sampleid]
+           illumina_gather_sample((sample_data, args.output_dir))
     else:
-        for sampleid in samples:
-            sample_data = df[df['sampleid'] == sampleid]
-            sample_bam2fq((sample_data, args.bam_home, args.output_dir))
+        print("ERROR: get_experiment.py unknown sequencing platform id:"+idtypes[0]); 
+        sys.exit(1)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
